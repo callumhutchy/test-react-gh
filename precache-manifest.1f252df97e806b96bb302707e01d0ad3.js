47f8f9000852e6fa353c1b471ee95281f6d8daff
Callum Hutchinson <callumhutchie@hotmail.co.uk>
2020-05-15 15:26:06 +0100

    Updates

---
+++ precache-manifest.1f252df97e806b96bb302707e01d0ad3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a2d0cd63ce5ab340e116b5c12d9ef5e",
+    "url": "/test-react-gh/index.html"
+  },
+  {
+    "revision": "d74c7603ed71e8ea44c6",
+    "url": "/test-react-gh/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "47c035eb47ff2495b019",
+    "url": "/test-react-gh/static/js/2.ad4c15ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-react-gh/static/js/2.ad4c15ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d74c7603ed71e8ea44c6",
+    "url": "/test-react-gh/static/js/main.a1639544.chunk.js"
+  },
+  {
+    "revision": "ce955644df169a7a1c3b",
+    "url": "/test-react-gh/static/js/runtime-main.27190676.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-react-gh/static/media/logo.5d5d9eef.svg"
+  }
+]);
